b3e8c11926a5720a304b034e16d11a875dbded99
Sung-june27 <kmymy27@gmail.com>
2024-12-12 14:02:09 +0900

    feat: 추천 메뉴 출력 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,10 +1,30 @@
 package menu.view;
 
+import java.util.List;
+import menu.domain.Coach;
+
 public class OutputView {
-    private static final String WELCOME_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    private final String WELCOME_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    private final String RESULT_TITLE = "메뉴 추천 결과입니다.\n[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]\n";
+    private final String CATEGORY_FORMAT = "[ 카테고리 | %s | %s | %s | %s | %s ]\n";
+    private final String MENU_FORMAT = "[ %s | %s | %s | %s | %s | %s ]\n";
 
     public void printWelcome() {
         System.out.println(WELCOME_MESSAGE);
+    }
+
+    public void printCategories(List<String> categories) {
+        System.out.print(RESULT_TITLE);
+        System.out.printf(CATEGORY_FORMAT, categories.get(0), categories.get(1), categories.get(2),
+                categories.get(3), categories.get(4));
+    }
+
+    public void printRecommendedMenu(List<Coach> coaches) {
+        for (Coach coach : coaches) {
+            List<String> recommendedFood = coach.getRecommendedFood();
+            System.out.printf(MENU_FORMAT, coach.getName(), recommendedFood.get(0), recommendedFood.get(1),
+                    recommendedFood.get(2), recommendedFood.get(3), recommendedFood.get(4));
+        }
     }
 
     public void printNewLine() {
